e76fc97ddb2d028b69780818a81ef9cc928d62b1
Cees Jol <cjol99@icloud.com>
2018-09-17 08:54:25 +0200

    Remove message test comments

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -4,10 +4,6 @@ var {generateMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
-    // store res in variable
-    // assert from match
-    // assert text match
-    // assert createdAt is a number
     var from = 'Cees';
     var text = 'Some message';
     var myMessage = generateMessage(from, text);
