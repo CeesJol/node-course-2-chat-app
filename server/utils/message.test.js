981b82b3884f3f137df5016e3e8dce2e0088583a
Cees Jol <cjol99@icloud.com>
2018-10-15 08:22:36 +0200

    Add geolocation support via gelocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -10,6 +10,21 @@ describe('generateMessage', () => {
 
     expect(myMessage.from).toEqual(from);
     expect(myMessage.text).toEqual(text);
+    expect(myMessage.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Cees';
+    var latitude = 69;
+    var longitude = 42;
+    var url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    var myMessage = generateLocationMessage(from, latitude, longitude);
+
+    // expect(myMessage.from).toEqual(from);
+    // expect(myMessage.url).toEqual(url);
+    expect(myMessage).toInclude({from, url});
     expect(myMessage.createdAt).toBeA('number');
   });
 });
